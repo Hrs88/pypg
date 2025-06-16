e3c7d5a1678d5b152060eeb26325e0a044b5dd88
Hrs88 <hh153129741@163.com>
2025-06-16 20:08:06 +0800

    繁花曲线done

--- spirograph/main.py
+++ spirograph/main.py
@@ -2,6 +2,9 @@ import sys
 import turtle
 import math
 import random
+from datetime import datetime
+import PIL.Image as Image
+import argparse
 class Spiro(object):
     '''
     用来绘制单个繁花曲线，且可复用
@@ -11,7 +14,7 @@ class Spiro(object):
         self.t.hideturtle()
         self.step = 1
         self.DrawComplete = False
-        self.SetParams(x,y,R,r,l,col)
+        self.SetParams(x,y,int(R),int(r),float(l),col)
         self.restart()
     def SetParams(self,x,y,R,r,l,col):
         '''
@@ -63,6 +66,7 @@ class SpiroAnimator(object):
         for i in range(N):
             self.spiros.append(Spiro(*self.GetRandomParams()))
     def GetRandomParams(self):
+        """获取随机繁花曲线参数"""
         R = random.randint(50,min(self.width,self.height)//2)
         r = random.randint(10,9*R//10)
         l = random.uniform(0.1,0.9)
@@ -76,6 +80,7 @@ class SpiroAnimator(object):
         return (x,y,R,r,l,col)
         # return (0,0,220,65,0.8,(0,0,0))
     def update(self):
+        """逐步画"""
         NumOfSpiros = 0
         for spiro in self.spiros:
             spiro.update()
@@ -85,15 +90,38 @@ class SpiroAnimator(object):
             self.restart()
         return True
     def restart(self):
+        """清空画布，重新随机生成繁花曲线参数"""
         for spiro in self.spiros:
             spiro.clear()
         self.spiros = []
         for i in range(random.randint(1,10)):
             self.spiros.append(Spiro(*self.GetRandomParams()))
+def SaveDrawing():
+    """
+    保存图片至png格式
+    依靠Ghostscript，如果没有会出问题
+    未测试
+    """
+    DateStr = datetime.now().strftime("%d-%b-%Y--%H-%M-%S")
+    FileName = "spiro-" + DateStr
+    Canvas = turtle.getcanvas()
+    Canvas.postscript(file=FileName+".eps")
+    Img = Image.open(FileName+".eps")
+    Img.save(FileName+".png","png")
 def main():
-    sa = SpiroAnimator(2)
-    while sa.update():
-        pass
+    ArgParser = argparse.ArgumentParser()
+    ArgParser.add_argument("--sparams",nargs=3,dest="sparams",required=False,help="输入R、r、l") # 设定参数
+    args = ArgParser.parse_args() # 分析参数
+    turtle.setup(width=0.8, height=0.8)
+    turtle.title("Spirograph")
+    turtle.onkey(SaveDrawing,"s") # 按s保存图片
+    turtle.listen()
+    if args.sparams:
+        s = Spiro(0,0,*[float(x) for x in args.sparams],(0,0,0))
+        s.Draw()
+    else:
+        sa = SpiroAnimator(random.randint(1,10))
+        while sa.update(): pass
     turtle.exitonclick()
 if __name__ == '__main__':
     main()
