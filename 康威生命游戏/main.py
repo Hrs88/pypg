c96e398d130abcc6867d0411158bed93433fd26c
Hrs88 <hh153129741@163.com>
2025-06-24 16:32:49 +0800

    康威生命游戏

--- 康威生命游戏/main.py
+++ 康威生命游戏/main.py
@@ -5,28 +5,42 @@
 4.如果一个元胞是死的，且活的邻接元胞数为3，那么它将复活
 """
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("n",help="参数的描述信息") # 位置参数，必须按位置传入
-parser.add_argument("-p",type=int) # 带 - 或者 -- 的参数，可选参数，传入前必须加上参数名 | 参数必须是int
-parser.add_argument("-e",default=False) # 参数默认值为False
-parser.add_argument("-d",required=True) # 只对可选参数有效，标识该参数是必须的
-parser.add_argument("-f",action="store")
-"""
-定义参数的行为，常见的有:
-    store 默认，存储参数的值
-    store_true 如果参数存在，存储true,参数不存在存储false
-    store_false 如果参数存在，存储true,参数不存在存储false
-    append 如果参数多次出现，就用列表存储
-    count 计算参数出现的次数
-"""
-parser.add_argument("-c",nargs=2) # 规定接收两个值 ?:0或1个值 *:0或多个值 +:1或多个值
-parser.add_argument("-a",dest="a") # 分析出来后，用来存储参数的成员变量名
-parser.add_argument("-b",choices=["x","y","z"]) # 限制参数只能是x、y、z其中的值。任意可迭代对象都可以传入
-parser.add_argument("--version",action="version",version="%(prog)s 1.0")
-# 显示版本信息,%(prog)s代表程序名称，即文件名
-parser.add_argument("-j",action="store_true")
-
-
-
-
-
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib import pyplot as plt
+from matplotlib import animation
+def GetRandomGrid(N):
+    """获取随机矩阵"""
+    return np.random.choice([0,1], N*N, p=[0.5,0.5]).reshape(N,N)
+def update(frames,img,grid,n):
+    newGrid = grid.copy()
+    for i in range(n):
+        for j in range(n):
+            total = (grid[(i-1)%n,(j-1)%n] + grid[(i-1)%n,j] + grid[(i-1)%n,(j+1)%n] +
+                     grid[i,(j-1)%n] + grid[i,(j+1)%n] +
+                     grid[(i+1)%n,(j-1)%n] + grid[(i+1)%n,j] + grid[(i+1)%n,(j+1)%n])
+            if newGrid[i,j] == 1:
+                if total == 2 or total == 3: pass
+                else: newGrid[i,j] = 0
+            else:
+                if total == 3: newGrid[i,j] = 1
+                else: pass
+    img.set_data(newGrid)  # 更新数据
+    grid[:] = newGrid[:]
+    return img,
+def main():
+    # 接收参数并分析参数
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n",default=10, type=int,required=False,dest="n",help="正方形网格的边长")
+    parser.add_argument("-i",default=500,type=int,required=False,dest="i",help="时间刻长度")
+    args = parser.parse_args()
+    # 创建随机矩阵
+    grid = GetRandomGrid(args.n)
+    # 设置动画
+    fig, ax = plt.subplots()
+    img = ax.imshow(grid)
+    ani = animation.FuncAnimation(fig,update,fargs=(img,grid,args.n),frames=10,interval=args.i)
+    plt.show()
+if __name__ == '__main__':
+    main()
